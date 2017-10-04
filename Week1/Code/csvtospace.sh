e3e22f2e9106bd4a2645aef1afedbc2618b31cff
Hira Tanvir <hira93@dyn1196-21.wlan.ic.ac.uk>
2017-10-04 10:56:22 +0100

    First commit

---
+++ Week1/Code/csvtospace.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Your name you.login@imperial.ac.uk
+# Script: csvtospace.sh
+# Desc: Coverts comma separated values to a space separated values file
+# saves the output into a .txt file
+# Arguments: 1-> tab delimited file
+# Date: Oct 2017
+echo "Create space delimited version of $1 ..."
+cat $1 | tr -s "," " " >> $1.txt
+echo "Done!"
+exit
